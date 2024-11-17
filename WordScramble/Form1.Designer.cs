72ccc7110c7fa003c089194a163a19e0dd0f3ee2
OpposedScroll75 <146241001+OpposedScroll75@users.noreply.github.com>
2024-11-17 18:10:23 +0100

    Add files via upload
    Initial version

---
+++ WordScramble/Form1.Designer.cs
@@ -0,0 +1,233 @@
+﻿namespace WordScramble
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.textBox6 = new System.Windows.Forms.TextBox();
+            this.textBox7 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.lbl_GuessedWords = new System.Windows.Forms.Label();
+            this.tmr_Game_Time = new System.Windows.Forms.Timer(this.components);
+            this.lbl_time = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)));
+            this.textBox1.Location = new System.Drawing.Point(352, 236);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(58, 20);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // button1
+            // 
+            this.button1.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.button1.Location = new System.Drawing.Point(291, 285);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Guess";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.button2.Location = new System.Drawing.Point(396, 285);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Scramble";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)));
+            this.textBox2.Location = new System.Drawing.Point(261, 176);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.ReadOnly = true;
+            this.textBox2.Size = new System.Drawing.Size(25, 20);
+            this.textBox2.TabIndex = 3;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)));
+            this.textBox3.Location = new System.Drawing.Point(307, 176);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.ReadOnly = true;
+            this.textBox3.Size = new System.Drawing.Size(25, 20);
+            this.textBox3.TabIndex = 4;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)));
+            this.textBox4.Location = new System.Drawing.Point(396, 176);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.ReadOnly = true;
+            this.textBox4.Size = new System.Drawing.Size(25, 20);
+            this.textBox4.TabIndex = 5;
+            // 
+            // textBox5
+            // 
+            this.textBox5.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)));
+            this.textBox5.Location = new System.Drawing.Point(352, 176);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.ReadOnly = true;
+            this.textBox5.Size = new System.Drawing.Size(25, 20);
+            this.textBox5.TabIndex = 6;
+            // 
+            // textBox6
+            // 
+            this.textBox6.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)));
+            this.textBox6.Location = new System.Drawing.Point(437, 176);
+            this.textBox6.Name = "textBox6";
+            this.textBox6.ReadOnly = true;
+            this.textBox6.Size = new System.Drawing.Size(25, 20);
+            this.textBox6.TabIndex = 7;
+            // 
+            // textBox7
+            // 
+            this.textBox7.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)));
+            this.textBox7.Location = new System.Drawing.Point(479, 176);
+            this.textBox7.Name = "textBox7";
+            this.textBox7.ReadOnly = true;
+            this.textBox7.Size = new System.Drawing.Size(25, 20);
+            this.textBox7.TabIndex = 8;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(42, 13);
+            this.label1.TabIndex = 9;
+            this.label1.Text = "Points: ";
+            // 
+            // button3
+            // 
+            this.button3.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.button3.Location = new System.Drawing.Point(735, 402);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(53, 20);
+            this.button3.TabIndex = 10;
+            this.button3.Text = "Quit";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.button4.Location = new System.Drawing.Point(15, 402);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(72, 20);
+            this.button4.TabIndex = 11;
+            this.button4.Text = "Play Again";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Visible = false;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // lbl_GuessedWords
+            // 
+            this.lbl_GuessedWords.AutoSize = true;
+            this.lbl_GuessedWords.Location = new System.Drawing.Point(34, 54);
+            this.lbl_GuessedWords.Name = "lbl_GuessedWords";
+            this.lbl_GuessedWords.Size = new System.Drawing.Size(0, 13);
+            this.lbl_GuessedWords.TabIndex = 12;
+            // 
+            // tmr_Game_Time
+            // 
+            this.tmr_Game_Time.Interval = 1000;
+            this.tmr_Game_Time.Tick += new System.EventHandler(this.tmr_Game_Time_Tick);
+            // 
+            // lbl_time
+            // 
+            this.lbl_time.AutoSize = true;
+            this.lbl_time.Location = new System.Drawing.Point(717, 54);
+            this.lbl_time.Name = "lbl_time";
+            this.lbl_time.Size = new System.Drawing.Size(0, 13);
+            this.lbl_time.TabIndex = 13;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lbl_time);
+            this.Controls.Add(this.lbl_GuessedWords);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox7);
+            this.Controls.Add(this.textBox6);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "Word Scramble";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.TextBox textBox6;
+        private System.Windows.Forms.TextBox textBox7;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Label lbl_GuessedWords;
+        private System.Windows.Forms.Timer tmr_Game_Time;
+        private System.Windows.Forms.Label lbl_time;
+    }
+}
+
---
+++ WordScramble/Form1.cs
@@ -0,0 +1,232 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WordScramble
+{
+    public partial class Form1 : Form
+    {
+        string[] correctWordList = { "Plan Late Pale Tale Net Planet", "Star Team Mate Arts Ram Stream", "Rode Doer Bore Red Rob Border", "Birth Grit Big Hit Rig Bright", "Farm Fear Mare Arms Sea Frames"};
+        string selectedWords = "";
+        string awardedWords = "";
+        string winningWord = "";
+        string guess = "";
+        int points;
+        int time = 30;
+        
+        int wordChooser;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Random ran = new Random();
+            tmr_Game_Time.Enabled = true;
+            wordChooser = ran.Next(0, 4);
+            lbl_time.Text = time.ToString();
+            selectedWords = correctWordList[wordChooser];
+            switch (wordChooser)
+            {
+                case 0:
+                    winningWord = "Planet";
+                    break;
+                case 1:
+                    winningWord = "Stream";
+                    break;
+                case 2:
+                    winningWord = "Border";
+                    break;
+                case 3:
+                    winningWord = "Bright";
+                    break;
+                case 4:
+                    winningWord = "Frames";
+                    break;
+            }
+            char[] winW = winningWord.ToCharArray();
+            Random r = new Random();
+
+            for (int i = winW.Length - 1; i >= 0; i--)
+            {
+                int randNbr = r.Next(0, winW.Length);
+                char temp = winW[i];
+                winW[i] = winW[randNbr];
+                winW[randNbr] = temp;
+            }
+
+            textBox2.Text = winW[0].ToString().ToLower();
+            textBox3.Text = winW[1].ToString().ToLower();
+            textBox4.Text = winW[2].ToString().ToLower();
+            textBox5.Text = winW[3].ToString().ToLower();
+            textBox6.Text = winW[4].ToString().ToLower();
+            textBox7.Text = winW[5].ToString().ToLower();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if(textBox1.Text != "")
+            {
+                guess = textBox1.Text;
+
+                if (selectedWords.Contains(guess) && guess.Length == 6)
+                {
+                    points += guess.Length;
+                    awardedWords += guess;
+                    lbl_GuessedWords.Text += $" {guess}";
+                    label1.Text = $"Points: {points}";
+                    time = 10000;
+                    lbl_time.Visible = false;
+                    MessageBox.Show("Congratulations, you won.");
+                    textBox1.Visible = false;
+                    button1.Visible = false;
+                    button2.Visible = false;
+                    button4.Visible = true;
+                    tmr_Game_Time.Enabled = false;
+                }
+
+                else if (selectedWords.Contains(guess))
+                {
+
+                    if (!awardedWords.Contains(guess))
+                    {
+                        points += guess.Length;
+                        awardedWords += guess;
+                        lbl_GuessedWords.Text += $" {guess}";
+                        label1.Text = $"Points: {points}";
+                    }
+                    else if (awardedWords.Contains(guess))
+                    {
+                        MessageBox.Show("You've already written that word");
+                        points += 0;
+                    }
+                }
+
+                else if (selectedWords.Contains(guess) == false)
+                {
+                    MessageBox.Show("Invalid word");
+                }
+            }
+
+            else if(textBox1.Text == "")
+            {
+                MessageBox.Show("Input field empty. Please write a word");
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            char[] winW = winningWord.ToCharArray();
+            Random r = new Random();
+
+            tmr_Game_Time.Enabled = true;
+            int time = 60;
+
+            for(int i = winW.Length - 1; i >= 0; i--)
+            {
+                int randNbr = r.Next(0, winW.Length);
+                char temp = winW[i];
+                winW[i] = winW[randNbr];
+                winW[randNbr] = temp;
+            }
+
+            textBox2.Text = winW[0].ToString().ToLower();
+            textBox3.Text = winW[1].ToString().ToLower();
+            textBox4.Text = winW[2].ToString().ToLower();
+            textBox5.Text = winW[3].ToString().ToLower();
+            textBox6.Text = winW[4].ToString().ToLower();
+            textBox7.Text = winW[5].ToString().ToLower();
+            
+        }
+
+        public void ResetValues()
+        {
+            Random ran = new Random();
+            wordChooser = ran.Next(0, 5);
+            selectedWords = correctWordList[wordChooser];
+            points = 0;
+            time = 30;
+            lbl_time.Visible = true;
+            lbl_time.Text = time.ToString();
+            tmr_Game_Time.Enabled = true;
+            awardedWords = "";
+            textBox1.Text = "";
+            label1.Text = "Points: ";
+            lbl_GuessedWords.Text = "";
+            textBox1.Visible = true;
+            button1.Visible = true;
+            button2.Visible = true;
+            button4.Visible = false;
+
+            switch (wordChooser)
+            {
+                case 0:
+                    winningWord = "Planet";
+                    break;
+                case 1:
+                    winningWord = "Stream";
+                    break;
+                case 2:
+                    winningWord = "Border";
+                    break;
+                case 3:
+                    winningWord = "Bright";
+                    break;
+                case 4:
+                    winningWord = "Frames";
+                    break;
+            }
+
+            char[] winW = winningWord.ToCharArray();
+            Random r = new Random();
+
+            for (int i = winW.Length - 1; i >= 0; i--)
+            {
+                int randNbr = r.Next(0, winW.Length);
+                char temp = winW[i];
+                winW[i] = winW[randNbr];
+                winW[randNbr] = temp;
+            }
+
+            textBox2.Text = winW[0].ToString().ToLower();
+            textBox3.Text = winW[1].ToString().ToLower();
+            textBox4.Text = winW[2].ToString().ToLower();
+            textBox5.Text = winW[3].ToString().ToLower();
+            textBox6.Text = winW[4].ToString().ToLower();
+            textBox7.Text = winW[5].ToString().ToLower();
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            ResetValues();
+        }
+
+        private void tmr_Game_Time_Tick(object sender, EventArgs e)
+        {
+            time--;
+            lbl_time.Text = time.ToString();
+
+            if(time == 0) {
+                tmr_Game_Time.Enabled = false;
+                MessageBox.Show("You ran out of time");
+                textBox1.Visible = false;
+                button1.Visible = false;
+                button2.Visible = false;
+                button4.Visible = true;
+            }
+        }
+    }
+}
